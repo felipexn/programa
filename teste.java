3cacbe7680dc5ce85af428ec4d0748f07a277f45
felipexn <felipexn.sv13@hotmail.com>
2023-04-12 23:56:07 -0300

    comit inicial

---
+++ teste.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class teste{
+public static void main(String[] args) {
+    Receita a;
+
+    Scanner entrada = new Scanner(System.in);
+    String oque = entrada.nextLine();
+
+    if(oque == "farofa"){
+        System.out.println(a.farofa);
+    }
+}
+}
